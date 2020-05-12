d78943dec00e57fce5a98101da5b0bd0fdcf6fe0
aishmn <manish.sinuwari@gmail.com>
2020-05-12 16:27:30 +0545

    test deploy

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
-const cookieParser = require('cookie-parser');
-const compression = require('compression');
-const cors = require('cors');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const cors = require("cors");
 
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-const userRouter = require('./routes/userRoutes');
-const profileRouter = require('./routes/profileRoutes');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const userRouter = require("./routes/userRoutes");
+const profileRouter = require("./routes/profileRoutes");
 
 // Start express app
 const app = express();
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
 app.use(cors());
 // Access-Control-Allow-Origin *
@@ -27,31 +27,30 @@ app.use(cors());
 //   origin: 'https://www.natours.com'
 // }))
 
-app.options('*', cors());
+app.options("*", cors());
 // app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Limit requests from same API
 const limiter = rateLimit({
   max: 1000,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
+  message: "Too many requests from this IP, please try again in an hour!",
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
@@ -63,14 +62,7 @@ app.use(xss());
 // Prevent parameter pollution
 app.use(
   hpp({
-    whitelist: [
-      'duration',
-      'ratingsQuantity',
-      'ratingsAverage',
-      'maxGroupSize',
-      'difficulty',
-      'price'
-    ]
+    whitelist: ["duration", "price"],
   })
 );
 
@@ -85,10 +77,15 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/profiles', profileRouter);
-
-app.all('*', (req, res, next) => {
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/profiles", profileRouter);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
